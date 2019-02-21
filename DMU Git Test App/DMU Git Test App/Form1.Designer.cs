036c3454c7b0ae2606a178c79eb8be04345c8abb
Wayne Taylor <wtaylor@leicestercollege.ac.uk>
2019-02-21 15:58:41 +0000

    Tidied things up

--- DMU Git Test App/DMU Git Test App/Form1.Designer.cs
+++ DMU Git Test App/DMU Git Test App/Form1.Designer.cs
@@ -30,9 +30,7 @@
         {
             this.pictureBoxMonkey1 = new System.Windows.Forms.PictureBox();
             this.buttonMessage = new System.Windows.Forms.Button();
-            this.pictureBoxMonkey2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMonkey1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMonkey2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxMonkey1
@@ -51,36 +49,23 @@
             this.buttonMessage.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonMessage.Location = new System.Drawing.Point(12, 236);
             this.buttonMessage.Name = "buttonMessage";
-            this.buttonMessage.Size = new System.Drawing.Size(406, 39);
+            this.buttonMessage.Size = new System.Drawing.Size(200, 39);
             this.buttonMessage.TabIndex = 1;
             this.buttonMessage.Text = "Click Here";
             this.buttonMessage.UseVisualStyleBackColor = true;
             this.buttonMessage.Click += new System.EventHandler(this.buttonMessage_Click);
-            // 
-            // buttonChangeColour
-            // 
-            this.buttonChangeColour.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonChangeColour.Location = new System.Drawing.Point(12, 281);
-            this.buttonChangeColour.Name = "buttonChangeColour";
-            this.buttonChangeColour.Size = new System.Drawing.Size(200, 39);
-            this.buttonChangeColour.TabIndex = 2;
-            this.buttonChangeColour.Text = "Change Colour";
-            this.buttonChangeColour.UseVisualStyleBackColor = true;
-            this.buttonChangeColour.Click += new System.EventHandler(this.buttonChangeColour_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.ClientSize = new System.Drawing.Size(224, 335);
-            this.Controls.Add(this.buttonChangeColour);
+            this.ClientSize = new System.Drawing.Size(224, 283);
             this.Controls.Add(this.buttonMessage);
             this.Controls.Add(this.pictureBoxMonkey1);
             this.Name = "Form1";
-            this.Text = "DMU Git Test - Monkeys";
+            this.Text = "DMU Git Test";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMonkey1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMonkey2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -89,7 +74,7 @@
 
         private System.Windows.Forms.PictureBox pictureBoxMonkey1;
         private System.Windows.Forms.Button buttonMessage;
-        private System.Windows.Forms.Button buttonChangeColour;
+        //private System.Windows.Forms.Button buttonChangeColour;
     }
 }
 
