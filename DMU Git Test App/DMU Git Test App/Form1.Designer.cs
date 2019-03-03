50d35e05146d8933f077a70f722ca8638a68c488
Marilu <marilugarciataylor@yahoo.co.uk>
2019-03-03 01:25:15 +0000

    Changed the background colour

--- DMU Git Test App/DMU Git Test App/Form1.Designer.cs
+++ DMU Git Test App/DMU Git Test App/Form1.Designer.cs
@@ -59,7 +59,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Green;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
             this.ClientSize = new System.Drawing.Size(224, 283);
             this.Controls.Add(this.buttonMessage);
             this.Controls.Add(this.pictureBoxMonkey1);
